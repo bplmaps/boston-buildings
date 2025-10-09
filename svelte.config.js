4d6fe499bbe7c95b1b01f1ded042f5e80b0ac516
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2025-10-09 14:03:54 -0400

    try another config

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,16 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
 
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
-};
+const config = {
+  kit: {
+    adapter: adapter({
+      edge: false,
+      split: false
+    })
+  },
+  preprocess: vitePreprocess(),
+}
 
 export default config;
