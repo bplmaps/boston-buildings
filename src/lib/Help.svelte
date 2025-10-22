7bb87d96c382220eca6c0c5c6235a31d43032369
lautenbach.k@northeastern.edu <katelautenbach452@gmail.com>
2025-10-22 16:41:03 -0400

    init

--- src/lib/Help.svelte
+++ src/lib/Help.svelte
@@ -1,24 +1,12 @@
 <script>
-  let { show } = $props();
-  function toggle() {
-    show = !show; console.log(show)
-  }
+  export let show;
 </script>
 
 <div class="absolute top-5 right-3">
   <button
-    onclick={toggle}
-    class=" bg-white rounded-full w-6 text-center select-none hover:cursor-pointer"
+    on:click={() => show = !show}
+    class="bg-white rounded-full w-6 text-center select-none hover:cursor-pointer"
   >
     ?
   </button>
 </div>
-
-{#if show}
-  <div class="absolute right-12 top-20 bg-white/90 p-2 rounded z-1000 w-136">
-    <p class="text-sm">This map shows the year of construction for all buildings in Boston(2024). The year built data comes from the city’s comprehensive <a href="https://data.boston.gov/dataset/boston-buildings-inventory">building inventory dataset</a>, which pulls from tax assessor records. Building shapes and locations were obtained from a <a href="https://www.mass.gov/info-details/massgis-data-building-structures-2-d#production">MassGIS dataset</a>. The map is useful for identifying citywide patterns in building ages, though individual records may contain inaccuracies since the data was originally collected for such specific analysis. Read more about the project <a href="https://leventhalmap.org">here</a>.
-    </p>
-    <p class="text-sm mt-2">
-    This project was inspired by Bert Spaan’s Waag Society <a href="https://code.waag.org/buildings/#52.3589,4.894,15">map of the Netherands </a> and Thomas Rhiel’s BKLYNR <a href="https://www.bklynr.com/block-by-block-brooklyns-past-and-present/">map of Brooklyn</a>.</p>
-  </div>
-{/if}
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,15 +3,15 @@
   let show = $state(false);
 </script>
 
-<nav class="bg-black py-1 font-sans border-b-1 border-white">
+<nav class="bg-black py-1 font-sans border-b border-white">
   <div class="max-w-7xl px-4 sm:px-6 lg:px-8">
-    <div class={`flex ${show ? "h-32" : "h-16"} items-center justify-between`}>
+    <div class={`flex ${show ? "h-18" : "h-16"} items-center justify-between`}>
       <div class="flex items-center space-x-2">
         <div class="mr-10">
           <a
             class="inline font-black text-red-400 hover:underline"
             href="https://leventhalmap.org"
-            target="blank"
+            target="_blank"
           >
             LMEC
           </a>
@@ -22,8 +22,28 @@
         </div>
       </div>
       <div class="h-16">
-        <Help {show} />
+        <Help bind:show />
       </div>
     </div>
+
+    {#if show}
+      <div class="text-white text-sm py-3 border-t border-gray-600 leading-snug">
+        <p>
+          This map shows the year of construction for all buildings in Boston(2024). The year built data comes 
+          from the city’s comprehensive <a href="https://data.boston.gov/dataset/boston-buildings-inventory">building 
+          inventory dataset</a>, which pulls from tax assessor records. Building shapes and locations were obtained 
+          from a <a href="https://www.mass.gov/info-details/massgis-data-building-structures-2-d#production">MassGIS 
+          dataset</a>. The map is useful for identifying citywide patterns in building ages, though individual records 
+          may contain inaccuracies since the data was originally collected for such specific analysis. Read more about 
+          the project <a href="https://leventhalmap.org">here</a>.
+        </p>
+        <p class="mt-2">
+          Inspired by Bert Spaan's Waag Society 
+          <a class="underline text-blue-300" href="https://code.waag.org/buildings/#52.3589,4.894,15">map of the Netherlands</a>
+          and Thomas Rhiel's BKLYNR 
+          <a class="underline text-blue-300" href="https://www.bklynr.com/block-by-block-brooklyns-past-and-present/">map of Brooklyn</a>.
+        </p>
+      </div>
+    {/if}
   </div>
 </nav>
