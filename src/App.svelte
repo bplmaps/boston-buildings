0f91f7191db139da95e48edbb09f42dd9f0d9f48
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2025-11-20 10:11:05 -0500

    fix the zoom depth

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,3 @@
-<!-- this was all made with boilerplate code from https://docs.maptiler.com/svelte/ -->
-
 <script>
   import Map from "./lib/Map.svelte";
   import Navbar from "./lib/Navbar.svelte";
